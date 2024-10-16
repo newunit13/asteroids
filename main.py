725a194ab4e9fc7b4fb25f7b518144f6a5096b23
Edward Russell II <edward@erco.dev>
2024-10-15 23:19:19 -0400

    updated main loop to run at 60 fps

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         
@@ -14,6 +16,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
