ade5c985a83110e65f524a6321b7ccc889143692
Edward Russell II <edward@erco.dev>
2024-10-16 00:05:35 -0400

    created groups and moved player

--- player.py
+++ player.py
@@ -5,6 +5,8 @@ from circleshape import CircleShape
 
 
 class Player(CircleShape):
+    containers = ()
+    
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
